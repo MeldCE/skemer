2232052c61c46f0331c22db52bb9f3b1575c5274
Jack Farley <jack@thesolderingbench.com>
2016-01-08 08:18:24 +0000

    - Started adding tests to test replace: true in schema

--- src/spec/types/object.spec.js
+++ src/spec/types/object.spec.js
@@ -104,6 +104,97 @@ var suites = [
 				}
 			}
 		]
+	},
+	{
+		pending: true,
+		label: 'Simple object containing string variables with replace set to true',
+		schema: {
+			type: {
+				test: {
+					type: 'string'
+				},
+				value: {
+					type: 'number'
+				},
+				flip: {
+					type: 'boolean'
+				}
+			},
+			replace: true
+		},
+		data: {
+			undef: undefined,
+			empty: {},
+			value: {
+				test: 'test',
+				value: 9
+			}
+		},
+		newData: {
+			undef: undefined,
+			invalid: 'notinarray',
+			empty: {},
+			value: {
+				test: 'string',
+				flip: true
+			}
+		},
+		options: clone(options),
+		results: [
+			{
+				input: ['undef', 'undef', false],
+				result: undefined
+			},
+			{
+				input: [false, 'invalid', 'empty'],
+				throws: new errors.DataTypeError('Value must be an object (string given)')
+			},
+			{
+				input: [
+					[false, 'empty', 'empty'],
+					[false, 'empty', 'global'],
+					[false, 'empty', 'specific']
+				],
+				result: {}
+			},
+			{
+				input: [['empty', 'undef', false], ['empty', 'empty', false]],
+				result: {}
+			},
+			{
+				input: [
+					['value', 'undef', false],
+					['value', 'empty', 'dont']
+				],
+				result: {
+					test: 'test',
+					value: 9
+				}
+			},
+			{
+				label: 'should merge values',
+				input: [
+					['value', 'value', 'dont']
+				],
+				result: {
+					test: 'string',
+					value: 9,
+					flip: true
+				}
+			},
+			{
+				label: 'should replace existing object',
+				input: [
+					['value', 'value', 'empty'],
+					['value', 'value', 'global'],
+					['value', 'value', 'specific'] 
+				],
+				result: {
+					test: 'string',
+					flip: true
+				}
+			}
+		]
 	}
 ];
 
